e0cde4596e1d6bf9729c9a4a14c7550c3b95e885
RBLU <reto.blunschi@youpers.com>
2017-02-08 06:00:22 +0100

    refactored to use real restify routes

---
+++ scripts/node-oracledb-prepare.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p ~/.oracle
+cd ~/.oracle
+wget https://retohome.youpers.org/oracleclient/instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+wget https://retohome.youpers.org/oracleclient/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+unzip instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip
+mv instantclient_12_1/ instantclient
+mv instantclient/libocci.so.12.1 instantclient/libocci.so
+mv instantclient/libclntsh.so.12.1 instantclient/libclntsh.so
