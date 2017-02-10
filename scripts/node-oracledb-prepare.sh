327be8d1e90bd03eea3c3378570278b3df467324
RBLU <reto.blunschi@youpers.com>
2017-02-10 10:11:43 +0100

    include a passing test that determines that indeed we do not have a database available on ci for now ;-)

--- scripts/node-oracledb-prepare.sh
+++ scripts/node-oracledb-prepare.sh
@@ -12,7 +12,7 @@ else
     wget https://hc.youpers.com/assets/instantclient-sdk-linux.x64-12.1.0.2.0.zip
     unzip instantclient-basiclite-linux.x64-12.1.0.2.0.zip
     unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip
-    mv instantclient_12_1/ instantclient
+    mv instantclient_12_1 instantclient
     mv instantclient/libocci.so.12.1 instantclient/libocci.so
     mv instantclient/libclntsh.so.12.1 instantclient/libclntsh.so
 fi
